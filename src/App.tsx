713b6fd6ee92df1f663af4026ea62079e4b64682
Dominik Zangerl <dominik.zangerl@student.uibk.ac.at>
2021-04-06 22:03:46 +0200

    UI layout, config, cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+//import React from 'react';
 import './App.css';
+import Box from '@material-ui/core/Box';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Header
       </header>
+      <body className="App-body">
+        <Box width="19.75%" mr="0.5%" height="100%">  
+          <Box height="50%" mb="0.5%" className="App-cpustate">
+            Register
+          </Box>
+          <Box height="29.75%" mb="0.5%" className="App-debugger">
+            Debugger
+          </Box>
+          <Box height="19.75%" className="App-options">
+            Options
+          </Box>
+        </Box>
+        <Box width="79.75%" height="100%">
+          <Box height="79.5%" mb="0.5%" className="App-textbox">
+            Input Textbox
+          </Box>
+          <Box height="19.6%"  className="App-terminal">
+            Terminal
+          </Box>
+        </Box>
+      </body>
     </div>
   );
 }
